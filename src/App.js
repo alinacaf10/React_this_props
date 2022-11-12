815af9b427e7dedd85f90b129423ea35acc79711
alinacaf10 <100665352+alinacaf10@users.noreply.github.com>
2022-11-12 22:43:43 +0400

    add_like_img

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import Wine from "./Wine";
 
-
-class App extends Component{
-  render(){
-    return(<>
-      <Wine
-      title="Chery"
-      description="French, White, 1l"
-      rating="4.8"
-      />
-      <Wine
-      title="Granate"
-      description="Italian, Red, 0.7l"
-      rating="4.2"
-      />
-</>
-    )
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Wine title="Chery" description="French, White, 1l" rating="4.8" />
+        <Wine title="Granate" description="Italian, Red, 0.7l" rating="4.2" like="false" />
+      </>
+    );
   }
 }
-export default App
+export default App;
--- src/Wine.js
+++ src/Wine.js
@@ -1,15 +1,20 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import "./wine.css";
 
-class Wine extends Component{
-  render(){
-    return(
+class Wine extends Component {
+  render() {
+    let like=<img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/red-heart_2764-fe0f.png" alt="like"/>;
+    if(this.props.like){
+      like=""
+    }
+    return (
       <>
-      <h3>{this.props.title}</h3>
-      <p className="desc">{this.props.description}</p>
-      <p className="rate">{this.props.rating}</p>
+        <h3>{this.props.title}</h3>
+        <p className="desc">{this.props.description}</p>
+        <p className="rate" type="number">{this.props.rating}</p>
+        <p>{like}</p>
       </>
-    )
+    );
   }
 }
-export default Wine
+export default Wine;
